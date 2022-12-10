1874715990d0bdaec799467b24fafda66aff7699
Jesse Tipton <jtipton@eero.com>
2022-12-10 15:57:29 -0800

    Bump to Swift tools version 5.7

--- Package.swift
+++ Package.swift
@@ -1,14 +1,20 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.7
 
 import PackageDescription
 
 let package = Package(
     name: "swift-nice-things",
     products: [
-        .library(name: "NiceThings", targets: ["NiceThings"]),
+        .library(
+            name: "NiceThings",
+            targets: ["NiceThings"]
+        ),
     ],
     targets: [
         .target(name: "NiceThings"),
-        .testTarget(name: "NiceThingsTests", dependencies: ["NiceThings"]),
+        .testTarget(
+            name: "NiceThingsTests",
+            dependencies: ["NiceThings"]
+        ),
     ]
 )
